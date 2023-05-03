4149e925e5e9acdf7f98c8288ea75e906af3d88b
Soulpurr <92094765+Soulpurr@users.noreply.github.com>
2023-05-03 20:14:11 +0530

    f

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,7 @@ export default function Home({ blogs, profile, skills, projects }) {
   });
 
   const builder = imageUrlBuilder(client);
-  // const profile = {
-  //   title: "WorkWithHarry",
-  //   name: "Harry",
-  //   image: "https://insanebiography.com/wp-content/uploads/codewithharry_108099807_133124151765153_6863548870509034899_n-min-edited.jpg",
-  //   fbLink: "https://facebook.com/codewithharry",
-  //   twitterLink: "https://twitter.com/codewithharry",
-  //   instagramLink: "https://instagram.com/codewithharry",
-  // }
+  
 
   return (
     <>
@@ -485,17 +478,19 @@ export default function Home({ blogs, profile, skills, projects }) {
             <div className="mx-auto grid w-full grid-cols-1 gap-8 pt-12 sm:w-3/4 md:gap-10 lg:w-full lg:grid-cols-2">
               {projects.map((item) => {
                 return (
-                  <Link
-                    key={item}
-                    href="/"
+                  <div
                     className="mx-auto transform transition-all hover:scale-105 md:mx-0"
+                    key={item}
                   >
-                    <img
-                      src={builder.image(item.image).width(800).url()}
-                      className="w-full shadow"
-                      alt="portfolio image"
-                    />
-                  </Link>
+                    <div className="text-xl font-semibold">{item.title}</div>
+                    <Link href={item.url || "/"} className="">
+                      <img
+                        src={builder.image(item.image).width(800).url()}
+                        className="w-full shadow"
+                        alt="portfolio image"
+                      />
+                    </Link>
+                  </div>
                 );
               })}
               {/* <Link
@@ -528,276 +523,6 @@ export default function Home({ blogs, profile, skills, projects }) {
                   alt="portfolio image"
                 />
               </Link> */}
-            </div>
-          </div>
-
-          <div className="bg-grey-50" id="clients">
-            <div className="container py-16 md:py-20">
-              <div className="mx-auto w-full sm:w-3/4 lg:w-full">
-                <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
-                  My latest clients
-                </h2>
-                <div className="flex flex-wrap items-center justify-center pt-4 sm:pt-4">
-                  <span className="m-8 block">
-                    <img
-                      src="/assets/img/logo-coca-cola.svg"
-                      alt="client logo"
-                      className="mx-auto block h-12 w-auto"
-                    />
-                  </span>
-                  <span className="m-8 block">
-                    <img
-                      src="/assets/img/logo-apple.svg"
-                      alt="client logo"
-                      className="mx-auto block h-12 w-auto"
-                    />
-                  </span>
-
-                  <span className="m-8 block">
-                    <img
-                      src="/assets/img/logo-netflix.svg"
-                      alt="client logo"
-                      className="mx-auto block h-12 w-auto"
-                    />
-                  </span>
-
-                  <span className="m-8 block">
-                    <img
-                      src="/assets/img/logo-amazon.svg"
-                      alt="client logo"
-                      className="mx-auto block h-12 w-auto"
-                    />
-                  </span>
-
-                  <span className="m-8 block">
-                    <img
-                      src="/assets/img/logo-stripe.svg"
-                      alt="client logo"
-                      className="mx-auto block h-12 w-auto"
-                    />
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="container py-16 md:py-20" id="work">
-            <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
-              My work experience
-            </h2>
-            <h3 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
-              Here&apos;s what I did before freelancing
-            </h3>
-
-            <div className="relative mx-auto mt-12 flex w-full flex-col lg:w-2/3">
-              <span className="left-2/5 absolute inset-y-0 ml-10 hidden w-0.5 bg-grey-40 md:block"></span>
-
-              <div className="mt-8 flex flex-col text-center md:flex-row md:text-left">
-                <div className="md:w-2/5">
-                  <div className="flex justify-center md:justify-start">
-                    <span className="shrink-0">
-                      <img
-                        src="/assets/img/logo-spotify.svg"
-                        className="h-auto w-32"
-                        alt="company logo"
-                      />
-                    </span>
-                    <div className="relative ml-3 hidden w-full md:block">
-                      <span className="absolute inset-x-0 top-1/2 h-0.5 -translate-y-1/2 transform bg-grey-70"></span>
-                    </div>
-                  </div>
-                </div>
-                <div className="md:w-3/5">
-                  <div className="relative flex md:pl-18">
-                    <span className="absolute left-8 top-1 hidden h-4 w-4 rounded-full border-2 border-grey-40 bg-white md:block"></span>
-
-                    <div className="mt-1 flex">
-                      <i className="bx bxs-right-arrow hidden text-primary md:block"></i>
-                      <div className="md:-mt-1 md:pl-8">
-                        <span className="block font-body font-bold text-grey-40">
-                          Apr 2015 - Mar 2018
-                        </span>
-                        <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">
-                          Frontend Developer
-                        </span>
-                        <div className="pt-2">
-                          <span className="block font-body text-black">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Vestibulum mattis felis vitae risus pulvinar
-                            tincidunt. Nam ac venenatis enim.
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-8 flex flex-col text-center md:flex-row md:text-left">
-                <div className="md:w-2/5">
-                  <div className="flex justify-center md:justify-start">
-                    <span className="shrink-0">
-                      <img
-                        src="/assets/img/logo-microsoft.svg"
-                        className="h-auto w-32"
-                        alt="company logo"
-                      />
-                    </span>
-                    <div className="relative ml-3 hidden w-full md:block">
-                      <span className="absolute inset-x-0 top-1/2 h-0.5 -translate-y-1/2 transform bg-grey-70"></span>
-                    </div>
-                  </div>
-                </div>
-                <div className="md:w-3/5">
-                  <div className="relative flex md:pl-18">
-                    <span className="absolute left-8 top-1 hidden h-4 w-4 rounded-full border-2 border-grey-40 bg-white md:block"></span>
-
-                    <div className="mt-1 flex">
-                      <i className="bx bxs-right-arrow hidden text-primary md:block"></i>
-                      <div className="md:-mt-1 md:pl-8">
-                        <span className="block font-body font-bold text-grey-40">
-                          Mar 2018 - September 2019
-                        </span>
-                        <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">
-                          Software Engineer
-                        </span>
-                        <div className="pt-2">
-                          <span className="block font-body text-black">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Vestibulum mattis felis vitae risus pulvinar
-                            tincidunt. Nam ac venenatis enim.
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-8 flex flex-col text-center md:flex-row md:text-left">
-                <div className="md:w-2/5">
-                  <div className="flex justify-center md:justify-start">
-                    <span className="shrink-0">
-                      <img
-                        src="/assets/img/logo-fedex.svg"
-                        className="h-auto w-32"
-                        alt="company logo"
-                      />
-                    </span>
-                    <div className="relative ml-3 hidden w-full md:block">
-                      <span className="absolute inset-x-0 top-1/2 h-0.5 -translate-y-1/2 transform bg-grey-70"></span>
-                    </div>
-                  </div>
-                </div>
-                <div className="md:w-3/5">
-                  <div className="relative flex md:pl-18">
-                    <span className="absolute left-8 top-1 hidden h-4 w-4 rounded-full border-2 border-grey-40 bg-white md:block"></span>
-
-                    <div className="mt-1 flex">
-                      <i className="bx bxs-right-arrow hidden text-primary md:block"></i>
-                      <div className="md:-mt-1 md:pl-8">
-                        <span className="block font-body font-bold text-grey-40">
-                          October 2019 - Feb 2021
-                        </span>
-                        <span className="block pt-2 font-header text-xl font-bold uppercase text-primary">
-                          DevOps Engineer
-                        </span>
-                        <div className="pt-2">
-                          <span className="block font-body text-black">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Vestibulum mattis felis vitae risus pulvinar
-                            tincidunt. Nam ac venenatis enim.
-                          </span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className="bg-cover bg-top bg-no-repeat pb-16 md:py-16 lg:py-24"
-            style={{
-              backgroundImage: "url(/assets/img/experience-figure.png)",
-            }}
-            id="statistics"
-          >
-            <div className="container">
-              <div className="mx-auto w-5/6 bg-white py-16 shadow md:w-11/12 lg:py-20 xl:py-24 2xl:w-full">
-                <div className="grid grid-cols-2 gap-5 md:gap-8 xl:grid-cols-4 xl:gap-5">
-                  <div className="flex flex-col items-center justify-center text-center md:flex-row md:text-left">
-                    <div>
-                      <img
-                        src="/assets/img/icon-project.svg"
-                        className="mx-auto h-12 w-auto md:h-20"
-                        alt="icon project"
-                      />
-                    </div>
-                    <div className="pt-5 md:pl-5 md:pt-0">
-                      <h1 className="font-body text-2xl font-bold text-primary md:text-4xl">
-                        12
-                      </h1>
-                      <h4 className="text-grey-dark font-header text-base font-medium leading-loose md:text-xl">
-                        Finished Projects
-                      </h4>
-                    </div>
-                  </div>
-
-                  <div className="flex flex-col items-center justify-center text-center md:flex-row md:text-left">
-                    <div>
-                      <img
-                        src="/assets/img/icon-award.svg"
-                        className="mx-auto h-12 w-auto md:h-20"
-                        alt="icon award"
-                      />
-                    </div>
-                    <div className="pt-5 md:pl-5 md:pt-0">
-                      <h1 className="font-body text-2xl font-bold text-primary md:text-4xl">
-                        3
-                      </h1>
-                      <h4 className="text-grey-dark font-header text-base font-medium leading-loose md:text-xl">
-                        Awards Won
-                      </h4>
-                    </div>
-                  </div>
-
-                  <div className="mt-6 flex flex-col items-center justify-center text-center md:mt-10 md:flex-row md:text-left lg:mt-0">
-                    <div>
-                      <img
-                        src="/assets/img/icon-happy.svg"
-                        className="mx-auto h-12 w-auto md:h-20"
-                        alt="icon happy clients"
-                      />
-                    </div>
-                    <div className="pt-5 md:pl-5 md:pt-0">
-                      <h1 className="font-body text-2xl font-bold text-primary md:text-4xl">
-                        8
-                      </h1>
-                      <h4 className="text-grey-dark font-header text-base font-medium leading-loose md:text-xl">
-                        Happy Clients
-                      </h4>
-                    </div>
-                  </div>
-
-                  <div className="mt-6 flex flex-col items-center justify-center text-center md:mt-10 md:flex-row md:text-left lg:mt-0">
-                    <div>
-                      <img
-                        src="/assets/img/icon-puzzle.svg"
-                        className="mx-auto h-12 w-auto md:h-20"
-                        alt="icon puzzle"
-                      />
-                    </div>
-                    <div className="pt-5 md:pl-5 md:pt-0">
-                      <h1 className="font-body text-2xl font-bold text-primary md:text-4xl">
-                        99
-                      </h1>
-                      <h4 className="text-grey-dark font-header text-base font-medium leading-loose md:text-xl">
-                        Bugs Fixed
-                      </h4>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
 
@@ -932,7 +657,7 @@ export default function Home({ blogs, profile, skills, projects }) {
                   </p>
                 </div>
                 <p className="pt-2 text-left font-body font-bold text-primary lg:text-lg">
-                  (+881) 111 222 333
+                  7840885186
                 </p>
               </div>
               <div className="w-full border-l-2 border-t-0 border-r-2 border-b-2 border-grey-60 px-6 py-6 sm:py-8 lg:w-1/3 lg:border-l-0 lg:border-t-2">
@@ -943,48 +668,9 @@ export default function Home({ blogs, profile, skills, projects }) {
                   </p>
                 </div>
                 <p className="pt-2 text-left font-body font-bold text-primary lg:text-lg">
-                  name@mydomain.com
+                  abhipoke@gmail.com
                 </p>
               </div>
-              <div className="w-full border-l-2 border-t-0 border-r-2 border-b-2 border-grey-60 px-6 py-6 sm:py-8 lg:w-1/3 lg:border-l-0 lg:border-t-2">
-                <div className="flex items-center">
-                  <i className="bx bx-map text-2xl text-grey-40"></i>
-                  <p className="pl-2 font-body font-bold uppercase text-grey-40 lg:text-lg">
-                    My Address
-                  </p>
-                </div>
-                <p className="pt-2 text-left font-body font-bold text-primary lg:text-lg">
-                  123 New York D Block 1100, 2011 USA
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className="h-72 bg-cover bg-center bg-no-repeat sm:h-64 md:h-72 lg:h-96"
-            style={{ backgroundImage: "url(/assets/img/map.png)" }}
-          ></div>
-
-          <div
-            className="relative bg-primary bg-cover bg-center bg-no-repeat py-16 bg-blend-multiply lg:py-24"
-            style={{ backgroundImage: "url(/assets/img/bg-cta.jpg)" }}
-          >
-            <div className="container relative z-30">
-              <h3 className="text-center font-header text-3xl uppercase leading-tight tracking-wide text-white sm:text-4xl lg:text-5xl">
-                Keep <span className="font-bold">up-to-date</span> <br />
-                with what I&apos;m up to
-              </h3>
-              <form className="mt-6 flex flex-col justify-center sm:flex-row">
-                <input
-                  className="w-full rounded px-4 py-3 font-body text-black sm:w-2/5 sm:py-4 lg:w-1/3"
-                  type="text"
-                  id="email"
-                  placeholder="Give me your Email"
-                />
-                <button className="mt-2 rounded bg-yellow px-8 py-3 font-body text-base font-bold uppercase text-primary transition-colors hover:bg-primary hover:text-white focus:border-transparent focus:outline-none focus:ring focus:ring-yellow sm:ml-2 sm:mt-0 sm:py-4 md:text-lg">
-                  Join the club
-                </button>
-              </form>
             </div>
           </div>
         </div>
@@ -994,20 +680,14 @@ export default function Home({ blogs, profile, skills, projects }) {
               © Copyright 2022. All right reserved, ATOM.
             </p>
             <div className="flex items-center justify-center pt-5 sm:justify-start sm:pt-0">
-              <Link href="/">
-                <i className="bx bxl-facebook-square text-2xl text-white hover:text-yellow"></i>
-              </Link>
-              <Link href="/" className="pl-4">
-                <i className="bx bxl-twitter text-2xl text-white hover:text-yellow"></i>
-              </Link>
-              <Link href="/" className="pl-4">
-                <i className="bx bxl-dribbble text-2xl text-white hover:text-yellow"></i>
-              </Link>
-              <Link href="/" className="pl-4">
+              <Link
+                href="https://www.linkedin.com/in/abhishek-ramola-b57509237/"
+                className="pl-4"
+              >
                 <i className="bx bxl-linkedin text-2xl text-white hover:text-yellow"></i>
               </Link>
-              <Link href="/" className="pl-4">
-                <i className="bx bxl-instagram text-2xl text-white hover:text-yellow"></i>
+              <Link href="https://github.com/Soulpurr" className="pl-4">
+                <i className="bx bxl-github text-2xl text-white hover:text-yellow"></i>
               </Link>
             </div>
           </div>
